37ce3eeef2dc3897d4baa24d1e916564b21db6a2
Zhe Hu <zhe@niatherapeutics.com>
2023-02-07 09:22:53 -0500

    p2

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,34 @@ fn main() -> Result<(), FtStatus> {
     let mut ft = Ftdi::new()?;
     ft.set_baud_rate(115200)?;
     ft.set_data_characteristics(BitsPerWord::Bits8, StopBits::Bits1, Parity::No)?;
-    let info = ft.device_info()?;
+    //let info = ft.device_info()?;
     //println!("Device information: {:?}", info);
 
     ft.purge_all()?;
 
-    const BUF_SIZE: usize = 100;
+    const BUF_SIZE: usize = 4;
     let mut buf: [u8; BUF_SIZE] = [0; BUF_SIZE];
     loop {
-        let _: usize = ft.read(&mut buf)?;
-        //println!("{}", bytes_read);
+        let _: usize = ft.read(&mut buf[0..1])?;
+        if buf[0] == 0xfe {
+            let _: usize = ft.read(&mut buf)?;
+            //println!("{}", bytes_read);
 
-        for x in buf.chunks(4) {
-            let ch1 = ((((x[0] as u32) << 8) + x[1] as u32) as f32) * 12.2e-3; //mV; 200mV range
-            let ch2 = ((((x[2] as u32) << 8) + x[3] as u32) as f32) * 12.2e-3; //mV; 200mV range
+            let ch1 = ((buf[0] as u16) << 8) + buf[1] as u16;
+            let ch2 = ((buf[2] as u16) << 8) + buf[3] as u16;
 
             println!("{},{}", ch1, ch2);
-            wtr.write_record(&[ch1.to_string(), ch2.to_string()]);
+            wtr.write_record(&[ch1.to_string(), ch2.to_string()]).unwrap();
+
+            wtr.flush().unwrap();
         }
-        wtr.flush();
+
         let keys: Vec<Keycode> = device_state.get_keys();
         if keys.contains(&Keycode::Q) {
             break;
         }
     }
-    wtr.flush();
+    wtr.flush().unwrap();
     ft.close()?;
     Ok(())
 }
