38865308aaa9dffa6349c61fc860136a72588125
Zhe Hu <zhe@niatherapeutics.com>
2023-03-01 11:57:43 -0500

    cmdline argh

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,21 @@ use corncobs::*;
 use device_query::{DeviceQuery, DeviceState, Keycode};
 use libftd2xx::{BitsPerWord, FtStatus, Ftdi, FtdiCommon, Parity, StopBits};
 
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// select E1 to E64 to stream out
+struct StreamData{
+    /// select single electrode data out: 1 to 64
+    #[argh(option, short='e', default="1")]
+    electrode: usize, // 1 to 64
+}
+
 fn main() -> Result<(), FtStatus> {
+    let stream: StreamData = argh::from_env();
+
+
+
     let device_state = DeviceState::new();
     let mut dt = chrono::offset::Local::now()
         .format("%Y-%m-%d-%H-%M-%S")
@@ -24,7 +38,6 @@ fn main() -> Result<(), FtStatus> {
     }
     let mut state = Fsm::RESYNC;
 
-    let mut serial_buffer_index = 0usize;
     let mut serial_buffer = [0u8; 400];
 
     const BUF_SIZE: usize = 33 * 4;
@@ -46,7 +59,6 @@ fn main() -> Result<(), FtStatus> {
                 if ft.queue_status()? > 0 {
                     let _: usize = ft.read(&mut header)?;
                     if header[0] != 0u8 {
-                        serial_buffer_index = 0;
                         serial_buffer[0] = header[0];
                         state = Fsm::COBS;
                         //println!("jmp to COBS with {serial_buffer_index}");
@@ -54,12 +66,11 @@ fn main() -> Result<(), FtStatus> {
                 }
             }
             Fsm::COBS => {
-                ft.read(&mut serial_buffer[1..134])?;
+                ft.read(&mut serial_buffer[1..134])?; // 33 * 4 = 132  + 2-byte extra from COBS
                 if serial_buffer[133] == 0 {
                     state = Fsm::IDLE;
                     let decoded_data_length =
-                        decode_buf(&serial_buffer[..134], &mut decoded_data)
-                            .unwrap();
+                        decode_buf(&serial_buffer[..134], &mut decoded_data).unwrap();
                     //    println!("decoded bytes = {decoded_data_length}");
                     if decoded_data_length == BUF_SIZE {
                         //got the right package size
@@ -68,9 +79,9 @@ fn main() -> Result<(), FtStatus> {
                             adc_data.push(
                                 (((decoded_data[i] as u16) << 8) + decoded_data[i + 1] as u16)
                                     .to_string(),
-                            ); // send as big-endian
+                            ); // receive as big-endian
                         }
-                        println!("{}", adc_data[16]);
+                        println!("{}", adc_data[stream.electrode-1]);
                         wtr.write_record(&adc_data).unwrap();
                         wtr.flush().unwrap();
                     } else {
@@ -80,42 +91,6 @@ fn main() -> Result<(), FtStatus> {
                     println!("error COBS");
                     state = Fsm::RESYNC;
                 }
-                // if ft.queue_status()? > 0 {
-                //     let _: usize = ft.read(&mut header)?;
-                //     serial_buffer_index += 1;
-                //     serial_buffer[serial_buffer_index] = header[0];
-                //     //println!("{serial_buffer_index}, {}", header[0]);
-                //     if header[0] == 0u8 {
-                //         state = Fsm::IDLE;
-                //         if serial_buffer_index == 133 {
-                //             let decoded_data_length = decode_buf(
-                //                 &serial_buffer[..=serial_buffer_index],
-                //                 &mut decoded_data,
-                //             )
-                //             .unwrap();
-                //         //    println!("decoded bytes = {decoded_data_length}");
-                //             if decoded_data_length == BUF_SIZE {
-                //                 //got the right package size
-                //                 let mut adc_data = Vec::new();
-                //                 for i in (0..BUF_SIZE).step_by(2) {
-                //                     adc_data.push(
-                //                         (((decoded_data[i] as u16) << 8)
-                //                             + decoded_data[i + 1] as u16)
-                //                             .to_string(),
-                //                     ); // send as big-endian
-                //                 }
-                //                 println!("{}", adc_data[1]);
-                //                 wtr.write_record(&adc_data).unwrap();
-                //                 wtr.flush().unwrap();
-                //             }else{
-                //                 println!("cobs error: decoded length={}", decoded_data_length);
-                //             }
-                //         } else {
-                //             println!("error: ser index  = {serial_buffer_index}");
-                //             // break;
-                //         }
-                //     }
-                // }
             }
         }
 
